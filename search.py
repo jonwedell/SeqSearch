02c86708f4f65676ca84eee07a954da926f75c0e
Jon Wedell <jonwedell@gmail.com>
2017-06-26 14:53:20 -0400

    Remove debugging comments.

--- search.py
+++ search.py
@@ -17,16 +17,14 @@ def contains(search):
     for seq in sequences.keys():
         if str1 in seq and str2 in seq:
             s1idx = [m.start() for m in re.finditer('(?=%s)' % str1, seq)]
-            #print("Found for %s in idx %s" % (sequences[seq], s1idx))
             for idx in s1idx:
                 start = idx+int(distance)+len(str1)
                 end = start + len(str2)
-                #print("comparing %s to %s" % (str2, seq[start:end]))
                 if seq[start:end] == str2:
-                    #print("FOUND! %s " % sequences[seq])
                     matches.extend(sequences[seq])
                     
     return sorted(list(set(matches)))
 
-for arg in sys.argv[1:]:
-    print("%s %s" % (arg, contains(arg)))
+if __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        print("%s %s" % (arg, contains(arg)))
