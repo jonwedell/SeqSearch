b8c7d285224bf802c3ca0efa01074849ac5b537b
Jon Wedell <jonwedell@gmail.com>
2017-06-26 14:39:28 -0400

    ->python3

--- search.py
+++ search.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+from __future__ import print_function
 
 import re
 import sys
@@ -15,16 +17,16 @@ def contains(search):
     for seq in sequences.keys():
         if str1 in seq and str2 in seq:
             s1idx = [m.start() for m in re.finditer('(?=%s)' % str1, seq)]
-            #print "Found for %s in idx %s" % (sequences[seq], s1idx)
+            #print("Found for %s in idx %s" % (sequences[seq], s1idx))
             for idx in s1idx:
                 start = idx+int(distance)+len(str1)
                 end = start + len(str2)
-                #print "comparing %s to %s" % (str2, seq[start:end])
+                #print("comparing %s to %s" % (str2, seq[start:end]))
                 if seq[start:end] == str2:
-                    #print "FOUND! %s " % sequences[seq]
+                    #print("FOUND! %s " % sequences[seq])
                     matches.extend(sequences[seq])
                     
     return sorted(list(set(matches)))
 
 for arg in sys.argv[1:]:
-    print "%s %s" % (arg, contains(arg))
+    print("%s %s" % (arg, contains(arg)))
